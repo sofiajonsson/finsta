ca42846c669562edfcfc2a48f409224e4ec2edbc
Sofia <jonssonsofia93@gmail.com>
2019-04-01 22:31:37 -0700

    created a finsta app

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+root to: 'home#index'
+  # get 'search/index'
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create, :show, :destroy]
+
+  get 'search' => 'search#index'
+end
